6d4a471116fa0717f74f34cb396c71ffc71a4580
Santiago Zarate <santiago.zarate@smartmatic.com>
2016-06-27 18:27:50 -0500

    Added some house cleanning, reduced the build dependencies
    Had to introduce a call to fuser -km since loopfile could be stalled
    sometimes (the root cause is still left to be found).
    
    Changed the boostrap variant to minbase to reduce the amount of
    packages being downloaded.
    
    Sort of cleaned a bit the script, to allow further refactoring.

--- bootstrap-os.sh
+++ bootstrap-os.sh
@@ -1,46 +1,66 @@
 #/bin/bash
-set -x
+
+function do_cleanup {
+
+  #housecleanning
+  losetup -d ${LOOPDEV}
+  fuser -km ${LOOPDEV}
+  rm ${IMAGE_FILE}
+
+}
+
+function do_umounts {
+
+  umount -l ${BOOTSTRAP_ROOTFS}/proc/
+  umount -l ${BOOTSTRAP_ROOTFS}/sys/
+  umount -l ${BOOTSTRAP_ROOTFS}/dev/
+
+  sync
+
+}
+
+trap do_cleanup SIGHUP SIGINT SIGTERM
+
 BOOTSTRAP_BASEDIR=${1:?"PARAMETER MISSING: You have to specify a base directory for rootfs and kernel build dir"}
 BOOTSTRAP_DATEPART=$(date +%d-%m-%y_%H-%M)
 ROOTFSBUILD_DIRECTORY="/tmp/os-images/debootstrap/builds"
 MODEL="3377"
 VERSION="PROTOTYPE"
 ARCH="i386"
-LOOPDEV=/dev/loop0
-IMAGE_FILE=$(mktemp -t IMAGE-XXXX)
+
 #KERNEL_SOURCES="/srv/os-resources/sources/linux-fslc-a3-arm-volar112-wandboard"
 
 if [[ ! -d ${ROOTFSBUILD_DIRECTORY}/${BOOTSTRAP_BASEDIR} ]]; then
 	mkdir -p ${ROOTFSBUILD_DIRECTORY}/${BOOTSTRAP_BASEDIR}
 fi;
 
-BOOTSTRAP_ROOTFS_REAL=$(mktemp -d --tmpdir=${ROOTFSBUILD_DIRECTORY}/${BOOTSTRAP_BASEDIR} -t "rootfs-${BOOTSTRAP_DATEPART}-XXX")
-BOOTSTRAP_ROOTFS=${ROOTFSBUILD_DIRECTORY}/${BOOTSTRAP_BASEDIR}/rootfs-latest
+export BOOTSTRAP_ROOTFS_REAL=$(mktemp -d --tmpdir=${ROOTFSBUILD_DIRECTORY}/${BOOTSTRAP_BASEDIR} -t "rootfs-${BOOTSTRAP_DATEPART}-XXX")
+export BOOTSTRAP_ROOTFS=${ROOTFSBUILD_DIRECTORY}/${BOOTSTRAP_BASEDIR}/rootfs-latest
+export IMAGE_FILE=$(mktemp -t IMAGE-XXXX)
+export LOOPDEV=/dev/loop0
 
 ln -sf ${BOOTSTRAP_ROOTFS_REAL} ${BOOTSTRAP_ROOTFS}
 
 DEPLOY_IMAGE=${ROOTFSBUILD_DIRECTORY}/${BOOTSTRAP_BASEDIR}/${MODEL}-${VERSION}-image-${BOOTSTRAP_DATEPART}.fsa
 DEPLOY_SCRIPT=${ROOTFSBUILD_DIRECTORY}/${BOOTSTRAP_BASEDIR}/prepare-image-media-model-version-${BOOTSTRAP_DATEPART}.sh
 
-umount -l ${LOOPDEV}
-
 dd if=/dev/zero of=${IMAGE_FILE} bs=1M count=2000
-losetup -f ${IMAGE_FILE}
-mkfs -t ext3 -F ${LOOPDEV} -L rootfs 
+losetup -f ${IMAGE_FILE}  || exit 256
+mkfs -t ext3 -F ${LOOPDEV} -L rootfs || exit 255
 mount -t ext3 -o noatime ${LOOPDEV} ${BOOTSTRAP_ROOTFS}
-
+ls ${BOOTSTRAP_ROOTFS}
 
 echo "Building everything on ${BOOTSTRAP_ROOTFS_REAL}"
 
-debootstrap \
-    --arch=${ARCH} \
+debootstrap 
+ \   --arch=${ARCH} \
     --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg \
     --verbose \
     --foreign \
-    --variant=buildd \
+    --variant=minbase \
     --no-check-gpg \
     --components=main,restricted,universe,multiverse \
-    --include=vim-nox,openssh-server,htop \
+    --include=vim-nox,openssh-server,htop,curl \
     trusty \
     ${BOOTSTRAP_ROOTFS} \
     http://pa.archive.ubuntu.com/ubuntu/
@@ -52,17 +72,10 @@ mount -t proc proc ${BOOTSTRAP_ROOTFS}/proc/
 mount -t sysfs sys ${BOOTSTRAP_ROOTFS}/sys/
 mount -o bind /dev ${BOOTSTRAP_ROOTFS}/dev/
 
-chroot ${BOOTSTRAP_ROOTFS} /bin/bash -c "/debootstrap/debootstrap --second-stage"
-
 #Make sure that apt installs what we really need instead of what it thinks
 
 
-
 LC_ALL=C chroot ${BOOTSTRAP_ROOTFS} /bin/bash -c '
-
-#Writting, this should be issued by an automatic way
-
-curl https://repogen.simplylinux.ch/txt/trusty/gpg_3ab636826b2777de98464dc2d8bca4d994b1a7a5.txt | sudo tee /etc/apt/gpg_keys.txt
 
 tee /etc/apt/apt.conf.d/01dpkgkeepconfiguration <<_EOF_
 APT::Install-Recommends "0";
@@ -82,12 +95,6 @@ Dpkg::Options {
 }
 _EOF_
 
-
-tee  /etc/apt/sources.list.d/sources.list <<_EOF_
-deb http://pa.archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse 
-deb http://algol-a.ve.sbp.com/packages/ trusty main restricted universe multiverse 
-_EOF_
-
 tee /etc/default/locale <<_EOF_ 
 LANG="en_US.UTF-8"
 LANGUAGE="en_US:en"
@@ -96,14 +103,23 @@ _EOF_
 tee /etc/tzdata <<_EOF_ 
 America/Panama
 _EOF_
+'
 
-echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
-apt-get --no-install-recommends -y --quiet install locales tzdata
+chroot ${BOOTSTRAP_ROOTFS} /bin/bash -c "/debootstrap/debootstrap --second-stage"
 
+LC_ALL=C chroot ${BOOTSTRAP_ROOTFS} /bin/bash -c '
 
-debconf locales locales/default_environment_locale select en_US.UTF-8 | debconf-set-selections
-debconf debconf/frontend select Noninteractive | debconf-set-selections
+#Writting, this should be issued by an automatic way
 
+curl https://repogen.simplylinux.ch/txt/trusty/gpg_3ab636826b2777de98464dc2d8bca4d994b1a7a5.txt | sudo tee /etc/apt/gpg_keys.txt
+
+echo debconf debconf/frontend select Noninteractive | debconf-set-selections
+echo debconf locales locales/default_environment_locale select en_US.UTF-8 | debconf-set-selections
+
+tee  /etc/apt/sources.list.d/sources.list <<_EOF_
+deb http://pa.archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse 
+deb http://algol-a.ve.sbp.com/packages/ trusty main restricted universe multiverse 
+_EOF_
 
 mount none /proc -t proc
 cd /dev
@@ -121,21 +137,16 @@ apt-get --install-recommends -y --quiet install linux-image-generic linux-firmwa
 rm /etc/apt/apt.conf.d/02Proxy
 '
 
-sync
-umount -l ${BOOTSTRAP_ROOTFS}/proc/
-umount -l ${BOOTSTRAP_ROOTFS}/sys/
-umount -l ${BOOTSTRAP_ROOTFS}/dev/
-sync
-
-#umount -l ${BOOTSTRAP_ROOTFS}
+do_umounts
 
 sync
 
 fsarchiver savefs -a -A -j4 -z9  ${DEPLOY_IMAGE} ${LOOPDEV}
+umount -l ${BOOTSTRAP_ROOTFS}
+
+do do_cleanup
 
 cp ${DEPLOY_IMAGE} .
 
-losetup -d ${LOOPDEV}
 
-echo "Your system is ready, you can find it in \n\t${BOOTSTRAP_ROOTFS_REAL}\nand the image files are found under \n\t${DEPLOY_IMAGE}"
-
+echo -e "Your system is ready, you can find it in \n\t${BOOTSTRAP_ROOTFS_REAL}\nand the image files are found under \n\t${DEPLOY_IMAGE}"
